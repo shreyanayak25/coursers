547620dc3e0777edb2e4a8fbbb70c725ede429b0
shreyanayak25 <65110422+shreyanayak25@users.noreply.github.com>
2020-05-23 16:13:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+(function() {
+    var names = ["Sachin", "Sehwag", "Ms Dhoni", "Steve", "Salman", "Rahul", "Rohit", "Raina", "Uttapa", "Sunil"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 's') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
